30d6913e5ee9562229a241c6dddb99e48e2ba0bc
Myroslava <myroslavs.krugla@gmail.com>
2019-02-18 22:53:31 +0300

    added EPAM sign

---
+++ src/com/groupqa1/main.java
@@ -0,0 +1,13 @@
+package com.groupqa1;
+
+public class main {
+    public static void main (String[] arg) {
+
+        System.out.println("**** ****     *     *   *");
+        System.out.println("*    *  *    * *    ** **");
+        System.out.println("**   ****   *   *   * * *");
+        System.out.println("*    *     *******  *   *");
+        System.out.println("**** *    *       * *   *");
+
+    }
+}
