477dd1599fe3d8e489644c775f6fb7d638b91bd2
Myroslava <myroslavs.krugla@gmail.com>
2019-03-01 16:19:03 +0300

    homework2. task1

---
+++ src/com/groupqa1/homework3/Main.java
@@ -0,0 +1,27 @@
+package com.groupqa1.homework3;
+
+public class Main {
+    public static void main (String[] args) {
+        int row = 1;
+        while (row < 6) {
+          int number = row;
+          while (number >=1) {
+              System.out.print("*");
+              number--;
+          }
+          row++;
+          System.out.println();
+        }
+        int row2 = 4;
+        while (row2 <=1) {
+            int number2 = row2;
+            while (number2 < 5) {
+                System.out.print("*");
+                number2--;
+            }
+            row2--;
+            System.out.println();
+        }
+
+    }
+}
