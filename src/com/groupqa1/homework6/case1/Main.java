c5bff212f171b832b052278158387f77e7cc5fed
Myroslava <myroslavs.krugla@gmail.com>
2019-03-20 23:20:33 +0300

    homework 6

--- src/com/groupqa1/homework6/case1/Main.java
+++ src/com/groupqa1/homework6/case1/Main.java
@@ -3,13 +3,8 @@ package com.groupqa1.homework6.case1;
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book();
-        book1.setID(123);
-        book1.setName("Pride and Prejudice");
-        book1.setAuthor("Jane Austen");
-        book1.setPages(563);
-        book1.setYear(1813);
-        book1.setCompany("Whitehall");
-        book1.setPrice(1000);
-        book1.view();
+        Books books = new Books(5);
+        books.addBook();
+        books.showAllBooks();
     }
 }
